b6b2c0c1e20b26f81d65878b7d86aa70b4aefcf7
PITSO98 <sqinisekomrbe@gmail.com>
2023-06-24 17:01:11 -0700

    tasks

--- mul.c
+++ mul.c
@@ -5,22 +5,22 @@
  * @stack: stack structure
  * @line_number: Number of instructions
  */
-void f_mul(stack_t **stack, unsigned int line_number)
+void f_mul(stack_t **head, unsigned int line_number)
 {
 	stack_t *temp = NULL;
 	int m;
 
-	m = stack_len(*stack);
+	m = stack_len(*head);
 	if (m < 2)
 	{
 		fprintf(stderr, "L%d: can't mul, stack too short\n", line_number);
 		if (list_opcode != NULL)
 			free_list_opcode(list_opcode);
-		if (*stack != NULL)
-			free_list_stack(*stack);
+		if (*head != NULL)
+			free_list_stack(*head);
 		exit(EXIT_FAILURE);
 	}
 
-	temp = *stack;
+	temp = *head;
 	temp->next->m *= temp->m;
-	pop(stack, line_number);
+	pop(head, line_number);
